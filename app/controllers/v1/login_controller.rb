6f5d03f58236622f8916468a7b09c788bbaa5297
Ivan <ivankuartz@hotmail.com>
2022-07-18 14:59:21 -0300

    Implement user login GET action

--- app/controllers/v1/login_controller.rb
+++ app/controllers/v1/login_controller.rb
@@ -1,7 +1,21 @@
 class V1::LoginController < ApplicationController
     def index
-        render json: {
-            error: "Testing an error message"
-        }
+        name = params[:name]
+        users = User.all
+        userPresent = false
+        users.each do |user|
+            if user.name == name
+                render json: {
+                    message: "Login successful."
+                }
+                userPresent = true
+                break
+            end
+        end
+        unless userPresent
+            render json: {
+                message: "User not present"
+            }
+        end
     end
 end
