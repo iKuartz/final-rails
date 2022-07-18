7c7296d9f10d4b3102bf1a1b6b3b62e88374299a
Ivan <ivankuartz@hotmail.com>
2022-07-18 14:33:19 -0300

    Add POST to login route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :v1, defaults: { format: 'json' } do
-    resources :login, only:[:index] do
+    resources :login, only:[:index,:create] do
     end
+    
   end
 end
