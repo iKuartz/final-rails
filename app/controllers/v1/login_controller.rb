e0718dcd9898d4ab4e3c63dc3cda009b26dda826
Saadat Ali <saadatlucky313@gmail.com>
2022-07-19 01:04:30 +0500

    Add create User action

--- app/controllers/v1/login_controller.rb
+++ app/controllers/v1/login_controller.rb
@@ -1,21 +1,42 @@
 class V1::LoginController < ApplicationController
-    def index
-        name = params[:name]
-        users = User.all
-        userPresent = false
-        users.each do |user|
-            if user.name == name
-                render json: {
-                    message: "Login successful."
-                }
-                userPresent = true
-                break
-            end
-        end
-        unless userPresent
-            render json: {
-                message: "User not present"
-            }
-        end
+  def index
+    name = params[:name]
+    users = User.all
+    user_present = false
+    users.each do |user|
+      next unless user.name == name
+
+      render json: {
+        message: 'Login successful.'
+      }
+      user_present = true
+      break
     end
+
+    return if user_present
+
+    render json: {
+      message: 'User not present'
+    }
+  end
+
+  def create
+    p register_params
+    p params
+    user = User.create(register_params)
+    if user.new_record?
+      render json: {
+        message: 'Error saving user to database',
+        error: user.errors[:name]
+      }
+    else
+      render json: {
+        message: 'User Created Successfully'
+      }
+    end
+  end
+
+  def register_params
+    params.require(:user).permit(:name)
+  end
 end
