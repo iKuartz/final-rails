4d83ba63a9412040c7c8e8b58ae62bdfd1f5350f
Ivan <ivankuartz@hotmail.com>
2022-07-19 18:02:05 -0300

    Correct linters

--- spec/requests/v1/login_spec.rb
+++ spec/requests/v1/login_spec.rb
@@ -1,28 +1,25 @@
 require 'swagger_helper'
 
 RSpec.describe 'Login', type: :request do
-
   path '/v1/login/{name}' do
-
     get('login user') do
       tags 'Login'
       consumes 'application/json'
       produces 'application/json'
-      parameter name: :name, in: :path, type: :string 
+      parameter name: :name, in: :path, type: :string
 
       response(200, 'successful') do
         schema type: :object,
-          properties: {
-            token: { type: :string },
-          },
-        required: [ 'token' ]
+               properties: {
+                 token: { type: :string }
+               },
+               required: ['token']
         run_test!
       end
     end
   end
 
   path '/v1/register' do
-
     post('register user') do
       response(200, 'registration successful') do
         after do |example|
