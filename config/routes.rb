071cdd772cf1af53469dfb4659efcbe1e1ffde8e
Saadat Ali <saadatlucky313@gmail.com>
2022-07-20 01:44:21 +0500

    Fix route for login get

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   namespace :v1, defaults: { format: 'json' } do
-    resources :login, only:[:index]
+    get '/login/:name', to: 'login#index'
     post '/register', to: 'login#create'
     resources :hotels, only: [:index]
   end
