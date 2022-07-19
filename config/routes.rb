31b2d70ba95bf877d4b9410c0d24bca9790eba04
Saadat Ali <saadatlucky313@gmail.com>
2022-07-19 21:18:43 +0500

    Add testing route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :v1, defaults: { format: 'json' } do
-    resources :login, only:[:index] do
-    end
-    post '/register', to: 'login#create'    
+    resources :login, only:[:index]
+    post '/register', to: 'login#create'
+    resources :hotels, only: [:index]
   end
 end
