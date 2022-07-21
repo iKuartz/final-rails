48fab108cd9e7e342d8f401f09c4a8d39d65b416
Ivan <ivankuartz@hotmail.com>
2022-07-21 20:26:09 -0300

    Update hotel model to add references to tables

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,6 +1,7 @@
 class Hotel < ApplicationRecord
-    has_one :feature
-    has_one :address
-    has_many :reservations
-    has_many :available_on_dates
+  has_one :feature
+  has_one :address
+  has_many :reservations
+  has_many :available_on_dates
+  belongs_to :user
 end
