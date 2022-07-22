bf8e077003af96d6861e1ec190e99e7eabacd225
Saadat Ali <saadatlucky313@gmail.com>
2022-07-22 22:55:30 +0500

    Fix associations and add validaion to hotel

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,7 +1,10 @@
 class Hotel < ApplicationRecord
-  has_one :feature
-  has_one :address
+  belongs_to :feature
+  belongs_to :address
   has_many :reservations
   has_many :available_on_dates
   belongs_to :user
+
+  validates :name, presence: { message: 'Name can\'t be blank' }
+  validates :description, presence: { message: 'Description should be given' }, length: { minimum: 20, maximum: 2000 }
 end
