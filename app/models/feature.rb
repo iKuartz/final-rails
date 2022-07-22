804ec240f3eb7cbe9f41b2dee2e5e9ee9ac9316f
Ivan <ivankuartz@hotmail.com>
2022-07-22 18:21:51 -0300

    Try correcting bug in feature.rb

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,10 +1,10 @@
 class Feature < ApplicationRecord
   has_one :hotel
   validates :room, numericality: { greater_than: 0, less_than: 9999 }
-  validates :pool, presence: { message: 'Feature pool is required.' }, inclusion: { in: [true, false] }
-  validates :bar, presence: { message: 'Feature bar is required.' }, inclusion: { in: [true, false] }
+  validates :pool, presence: { message: 'Feature pool is required.' }, inclusion: [true, false] 
+  validates :bar, presence: { message: 'Feature bar is required.' }, inclusion: [true, false] 
   validates :air_conditioning, presence: { message: 'Feature air conditioning is required.' },
-                               inclusion: { in: [true, false] }
-  validates :tv, presence: { message: 'Feature tv is required.' }, inclusion: { in: [true, false] }
-  validates :gym, presence: { message: 'Feature gym is required.' }, inclusion: { in: [true, false] }
+                               inclusion: [true, false] 
+  validates :tv, presence: { message: 'Feature tv is required.' }, inclusion: [true, false] 
+  validates :gym, presence: { message: 'Feature gym is required.' }, inclusion: [true, false] 
 end
