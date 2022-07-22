b99bd6444450ab1f9e15b88c788af13764b87a6f
Saadat Ali <saadatlucky313@gmail.com>
2022-07-22 22:55:58 +0500

    Fix associations and add validaion to feature

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,3 +1,9 @@
 class Feature < ApplicationRecord
-  belongs_to :hotel
+  has_one :hotel
+  validates :room, numercality: { greater_than: 0, less_than: 9999 }
+  validates :pool, presence: { message: 'Feature pool is required.' }, inclusion: { in: [true, false] }
+  validates :bar, presence: { message: 'Feature bar is required.' }, inclusion: { in: [true, false] }
+  validates :air_conditioning, presence: { message: 'Feature air conditioning is required.' }, inclusion: { in: [true, false] }
+  validates :tv, presence: { message: 'Feature tv is required.' }, inclusion: { in: [true, false] }
+  validates :gym, presence: { message: 'Feature gym is required.' }, inclusion: { in: [true, false] }
 end
