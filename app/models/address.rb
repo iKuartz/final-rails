03fece6016af294115d259cddd2e05a16f262fa0
Saadat Ali <saadatlucky313@gmail.com>
2022-07-23 01:25:38 +0500

    Fix validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,9 +1,9 @@
 class Address < ApplicationRecord
   has_one :hotel
-  validate :country, presence: { message: 'Country can not be blank' }
-  validate :state, presence: { message: 'State can not be blank' }
-  validate :city, presence: { message: 'City can not be blank' }
-  validate :neighbourhood, presence: { message: 'Neighbourhood can not be blank' }
-  validate :street, presence: { message: 'Street can not be blank' }
-  validate :number, presence: { message: 'Number can not be blank' }, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :country, presence: { message: 'Country can not be blank' }
+  validates :state, presence: { message: 'State can not be blank' }
+  validates :city, presence: { message: 'City can not be blank' }
+  validates :neighbourhood, presence: { message: 'Neighbourhood can not be blank' }
+  validates :street, presence: { message: 'Street can not be blank' }
+  validates :number, presence: { message: 'Number can not be blank' }, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
