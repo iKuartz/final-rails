fdf0ca1a5e48a89a2ea26331a158c27264ccce29
Saadat Ali <saadatlucky313@gmail.com>
2022-07-22 22:55:45 +0500

    Fix associations and add validaion to address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,9 @@
 class Address < ApplicationRecord
-  belongs_to :hotel
+  has_one :hotel
+  validate :country, presence: { message: 'Country can not be blank' }
+  validate :state, presence: { message: 'State can not be blank' }
+  validate :city, presence: { message: 'City can not be blank' }
+  validate :neighbourhood, presence: { message: 'Neighbourhood can not be blank' }
+  validate :street, presence: { message: 'Street can not be blank' }
+  validate :number, presence: { message: 'Number can not be blank' }, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
