8cb1d5ed30fd98584be756b7818899a6b3e4b9a9
Saadat Ali <saadatali0202@gmail.com>
2022-07-28 02:56:24 +0500

    Add index, create and destroy action

--- app/controllers/v1/reservation_controller.rb
+++ app/controllers/v1/reservation_controller.rb
@@ -1,2 +1,19 @@
 class ReservationController < ApplicationController
+  def index
+    render json: {
+      status: 'Under Construction'
+    }
+  end
+
+  def create
+    render json: {
+      status: 'Under Construction'
+    }
+  end
+
+  def destroy
+    render json: {
+      status: 'Under Construction'
+    }
+  end
 end
