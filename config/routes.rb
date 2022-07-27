624ffe83f9b777f9794801b9115ef06066baa4ef
Saadat Ali <saadatali0202@gmail.com>
2022-07-28 02:57:50 +0500

    Add routes for reservation and dates

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
     get '/login/:name', to: 'login#index'
     post '/register', to: 'login#create'
     resources :hotels, only: [:index, :create]
+    resources :reservation, only: [:create, :index, :destroy]
+    resources :dates, only: [:index]
   end
 end
