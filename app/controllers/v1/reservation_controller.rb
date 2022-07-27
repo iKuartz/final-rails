914639877c0bd99b6871537fa5dc7948810fe7c3
Saadat Ali <saadatali0202@gmail.com>
2022-07-28 03:35:19 +0500

    Get parameters from the post request

--- app/controllers/v1/reservation_controller.rb
+++ app/controllers/v1/reservation_controller.rb
@@ -6,14 +6,29 @@ class V1::ReservationController < ApplicationController
   end
 
   def create
-    render json: {
-      status: 'Under Construction'
-    }
+    token = request.headers['token']
+    secret = Rails.application.secret_key_base.to_s
+
+    begin
+      decoded_token = JWT.decode token, secret, true, { algorithm: 'HS256' }
+      decoded_username = decoded_token[0]['name']
+
+    rescue JWT::DecodeError
+      render json: {
+        error: 'Invalid Token'
+      }, status: 500
+    end
   end
 
   def destroy
     render json: {
       status: 'Under Construction'
     }
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:reserved_rooms, :hotel_id, :start_date, :end_date)
   end
 end
