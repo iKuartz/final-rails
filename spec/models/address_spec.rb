45970b2f73a467221c00ccba612fecf75f9f2930
Ivan <ivankuartz@hotmail.com>
2022-07-28 13:26:07 -0300

    Add address module tests

---
+++ spec/models/address_spec.rb
@@ -0,0 +1,120 @@
+require 'rails_helper'
+
+RSpec.describe Address, :type => :model do
+  it "is valid with valid attributes" do
+    address = Address.new(
+        hotel: @hotel,
+        country: "Brazil",
+        state:"Rio de Janeiro",
+        city:"Rio de Janeiro",
+        neighbourhood:"Copacabana",
+        street:"Barata Ribeiro",
+        number:100
+    )
+    expect(address).to be_valid
+  end
+  
+  it "is not valid without a country" do
+    address = Address.new(
+        hotel: @hotel,
+        country: nil,
+        state:"Rio de Janeiro",
+        city:"Rio de Janeiro",
+        neighbourhood:"Copacabana",
+        street:"Barata Ribeiro",
+        number:100
+    )
+    expect(address).to_not be_valid
+  end
+
+  it "is not valid without a state" do
+    address = Address.new(
+        hotel: @hotel,
+        country: "Brazil",
+        state: nil,
+        city:"Rio de Janeiro",
+        neighbourhood:"Copacabana",
+        street:"Barata Ribeiro",
+        number:100
+    )
+    expect(address).to_not be_valid
+  end
+
+  it "is not valid without a city" do
+    address = Address.new(
+        hotel: @hotel,
+        country: "Brazil",
+        state:"Rio de Janeiro",
+        city: nil,
+        neighbourhood:"Copacabana",
+        street:"Barata Ribeiro",
+        number:100
+    )
+    expect(address).to_not be_valid
+  end
+
+  it "is not valid without a neighbourhood" do
+    address = Address.new(
+        hotel: @hotel,
+        country: "Brazil",
+        state:"Rio de Janeiro",
+        city:"Rio de Janeiro",
+        neighbourhood: nil,
+        street:"Barata Ribeiro",
+        number:100
+    )
+    expect(address).to_not be_valid
+  end
+
+  it "is not valid without a street" do
+    address = Address.new(
+        hotel: @hotel,
+        country: "Brazil",
+        state:"Rio de Janeiro",
+        city:"Rio de Janeiro",
+        neighbourhood:"Copacabana",
+        street: nil,
+        number:100
+    )
+    expect(address).to_not be_valid
+  end
+
+  it "is not valid without a number" do
+    address = Address.new(
+        hotel: @hotel,
+        country: "Brazil",
+        state:"Rio de Janeiro",
+        city:"Rio de Janeiro",
+        neighbourhood:"Copacabana",
+        street:"Barata Ribeiro",
+        number: nil
+    )
+    expect(address).to_not be_valid
+  end
+
+  it "is not valid if number is not an integer" do
+    address = Address.new(
+        hotel: @hotel,
+        country: "Brazil",
+        state:"Rio de Janeiro",
+        city:"Rio de Janeiro",
+        neighbourhood:"Copacabana",
+        street:"Barata Ribeiro",
+        number: "Powerful Place"
+    )
+    expect(address).to_not be_valid
+  end
+
+  it "is not valid if number is negative" do
+    address = Address.new(
+        hotel: @hotel,
+        country: "Brazil",
+        state:"Rio de Janeiro",
+        city:"Rio de Janeiro",
+        neighbourhood:"Copacabana",
+        street:"Barata Ribeiro",
+        number: -100
+    )
+    expect(address).to_not be_valid
+  end
+end
