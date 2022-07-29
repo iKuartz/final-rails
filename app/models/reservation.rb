60c23cacc9668b53f14f10f1ee5b45d4728651db
Ivan <ivankuartz@hotmail.com>
2022-07-29 12:11:08 -0300

    Add validations to reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,12 @@
 class Reservation < ApplicationRecord
   belongs_to :hotel
   belongs_to :user
+  validates :reserved_rooms, numericality: { message: 'reserved_rooms must be a number', greater_than: 0 }
+  validates :date_from, presence: { message: 'Initial date can\'t be empty' }
+  validates :date_to, presence: { message: 'Final date can\'t be empty' }
+
+  # def validate_date
+  #   if date_from.class
+  #   end
+  # end
 end
