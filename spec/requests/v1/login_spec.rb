9296bd81b8c4d34a4f03cd03a83b69b1dc6bbcf5
Saadat Ali <saadatali0202@gmail.com>
2022-07-30 05:22:00 +0500

    Add documentation

--- spec/requests/v1/login_spec.rb
+++ spec/requests/v1/login_spec.rb
@@ -11,6 +11,53 @@ RSpec.describe 'Login', type: :request do
       expect(response).to have_http_status 500
     end
   end
+
+  path '/v1/login/{name}' do
+    get('Logs user in') do
+      description 'Logs the user based on user name if the user exists.'
+      tags 'Login Route'
+      produces 'application/json'
+      parameter name: 'name', in: :path, type: :string, description: 'Name of registered user'
+
+      response(200, 'User Present in database') do
+        schema(type: :object,
+               properties: {
+                 token: { type: :string }
+               })
+
+        let(:name) { 'Ivan' }
+        run_test!
+      end
+
+      response(404, 'User does not exist') do
+        schema type: :object,
+               properties: {
+                 message: { type: :string },
+                 error: { type: :string }
+               }
+
+        let(:name) { '123' }
+        run_test!
+      end
+    end
+  end
+
+  # path '/v1/register' do
+
+  #   post('create login') do
+  #     response(200, 'successful') do
+
+  #       after do |example|
+  #         example.metadata[:response][:content] = {
+  #           'application/json' => {
+  #             example: JSON.parse(response.body, symbolize_names: true)
+  #           }
+  #         }
+  #       end
+  #       run_test!
+  #     end
+  #   end
+  # end
   # path '/v1/login/{name}' do
   #   get('login user') do
   #     tags 'Login'
