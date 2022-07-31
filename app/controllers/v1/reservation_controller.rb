b497879a81a2405fa87f9e29a9e498827f1bda7d
Saadat Ali <saadatali0202@gmail.com>
2022-07-31 19:25:36 +0500

    Follow SOLID and DRY principle

--- app/controllers/v1/reservation_controller.rb
+++ app/controllers/v1/reservation_controller.rb
@@ -1,21 +1,16 @@
 # rubocop:disable Metrics/ClassLength
 class V1::ReservationController < ApplicationController
   def index
-    token = request.headers['token']
-    secret = Rails.application.secret_key_base.to_s
-    begin
-      decoded_token = JWT.decode token, secret, true, { algorithm: 'HS256' }
-      decoded_username = decoded_token[0]['name']
-      user = User.where(name: decoded_username).first
-      reservations = Reservation.where(user_id: user.id).as_json(except: %i[created_at updated_at user_id])
-      render json: {
-        reservations:
-      }
-    rescue JWT::DecodeError
-      render json: {
-        error: 'Invalid Token'
-      }, status: 500
-    end
+    decoded_username = JsonWebToken.decode request.headers['token']
+    user = User.where(name: decoded_username).first
+    reservations = Reservation.where(user_id: user.id).as_json(except: %i[created_at updated_at user_id])
+    render json: {
+      reservations:
+    }, status: 200
+  rescue JWT::DecodeError
+    render json: {
+      error: 'Invalid Token'
+    }, status: 500
   end
 
   def rooms_available_on_date(reserved_rooms, rooms_free)
@@ -26,7 +21,6 @@ class V1::ReservationController < ApplicationController
     available_dates_list = []
     (start_date..end_date).each do |date|
       date_list = available_on_date_for_hotel_list.select { |available| available.date == date }
-      puts date_list
       if date_list.length.zero?
         available_on_date_for_hotel = AvailableOnDate.new hotel_id: hotel.id, rooms_occopied: 0, date:, available: true,
                                                           rooms_free: hotel.feature.room
@@ -42,102 +36,92 @@ class V1::ReservationController < ApplicationController
   end
 
   def update_rooms_availability(available_dates_list, reserved_rooms)
-    ActiveRecord::Base.transaction do
-      available_dates_list.each do |available_date|
-        available_date.rooms_occopied += reserved_rooms.to_i
-        available_date.rooms_free -= reserved_rooms.to_i
-        available_date.available = available_date.rooms_free.positive?
-        available_date.save
-      end
+    available_dates_list.each do |available_date|
+      available_date.rooms_occopied += reserved_rooms.to_i
+      available_date.rooms_free -= reserved_rooms.to_i
+      available_date.available = available_date.rooms_free.positive?
+      available_date.save
     end
   end
 
   def process_rooms_availability(hotel_id, start_date, end_date, reserved_rooms)
     available_on_date_for_hotel_list = AvailableOnDate.where(hotel_id:, date: start_date..end_date)
     hotel = Hotel.find hotel_id
-    available_dates_list = []
-    begin
-      available_dates_list = check_availaibility start_date, end_date, available_on_date_for_hotel_list, reserved_rooms,
-                                                 hotel
-    rescue StandardError => e
-      raise StandardError, e.message
+    available_dates_list = check_availaibility start_date, end_date, available_on_date_for_hotel_list, reserved_rooms,
+                                               hotel
+
+    update_rooms_availability available_dates_list, reserved_rooms
+  end
+
+  def check_validations(start_date, end_date, reserved_rooms)
+    if start_date.past?
+      return {
+        error: 'Unable to create reservation[1x604]',
+        error_list: ['Can not have a date from past']
+      }
     end
 
-    begin
-      update_rooms_availability available_dates_list, reserved_rooms
-    rescue ActiveRecord::Rollback
-      raise StandardError, 'Unable to create reservation. ActiveRecord::Rollback [1x602]'
-    rescue ActiveRecord::StatementInvalid
-      raise StandardError, 'Unable to create reservation. ActiveRecord::StatementInvalid [1x603]'
+    if start_date > end_date
+      return {
+        error: 'Unable to create reservation[1x605]',
+        error_list: ['Start date can not be after the end date.']
+      }
     end
+
+    if reserved_rooms.to_i < 1
+      return {
+        error: 'Unable to create reservation[1x606]',
+        error_list: ['You should reserve at least one room.']
+      }
+    end
+    nil
+  end
+
+  def create_reservation(parameters, user)
+    Reservation.create user_id: user.id, hotel_id: parameters[:hotel_id],
+                       date_from: parameters[:start_date], date_to: parameters[:end_date],
+                       reserved_rooms: parameters[:reserved_rooms]
   end
 
   def create
-    token = request.headers['token']
-    secret = Rails.application.secret_key_base.to_s
+    decoded_username = JsonWebToken.decode request.headers['token']
+    user = User.where(name: decoded_username).first
+    parameters = reservation_params
+    start_date = Date.parse(parameters[:start_date])
+    end_date = Date.parse(parameters[:end_date])
 
-    begin
-      decoded_token = JWT.decode token, secret, true, { algorithm: 'HS256' }
-      decoded_username = decoded_token[0]['name']
-      user = User.where(name: decoded_username).first
-      parameters = reservation_params
-
-      start_date = Date.parse(parameters[:start_date])
-      end_date = Date.parse(parameters[:end_date])
-
-      if start_date.past?
-        render json: {
-          error: 'Unable to create reservation[1x604]',
-          error_list: ['Can not have a date from past']
-        }, status: 501
-        return
-      end
-
-      if start_date > end_date
-        render json: {
-          error: 'Unable to create reservation[1x605]',
-          error_list: ['Start date can not be after the end date.']
-        }, status: 501
-        return
-      end
-
-      if parameters[:reserved_rooms].to_i < 1
-        render json: {
-          error: 'Unable to create reservation[1x606]',
-          error_list: ['You should reserve at least one room.']
-        }, status: 501
-        return
-      end
-
-      reservation = Reservation.create user_id: user.id, hotel_id: parameters[:hotel_id],
-                                       date_from: parameters[:start_date], date_to: parameters[:end_date],
-                                       reserved_rooms: parameters[:reserved_rooms]
-      if reservation.new_record?
-        render json: {
-          error: 'Unable to create reservation[1x601]',
-          error_list: reservation.errors.full_messages
-        }, status: 501
-      else
-        begin
-          process_rooms_availability parameters[:hotel_id], parameters[:start_date], parameters[:end_date],
-                                     parameters[:reserved_rooms]
-        rescue StandardError => e
-          reservation.destroy
-          render json: {
-            error: 'Unable to create reservation. Internal Server Error.',
-            error_list: [e.message]
-          }, status: 501
-          return
-        end
-        render json: {
-          message: "Hotel reserved from #{reservation.date_from} to #{reservation.date_to}."
-        }, status: 200
-      end
-    rescue JWT::DecodeError
-      render json: {
-        error: 'Invalid Token'
-      }, status: 500
+    validation_check = check_validations(start_date, end_date, parameters[:reserved_rooms])
+    unless validation_check.nil?
+      render json: validation_check, status: 501
+      return
     end
+    ActiveRecord::Base.transaction do
+      reservation = create_reservation parameters, user
+      process_rooms_availability parameters[:hotel_id], parameters[:start_date], parameters[:end_date],
+                                 parameters[:reserved_rooms]
+      render json: {
+        message: "Hotel reserved from #{reservation.date_from} to #{reservation.date_to}."
+      }, status: 200
+    end
+  rescue JWT::DecodeError
+    render json: {
+      error: 'Invalid Token'
+    }, status: 500
+  rescue ActiveRecord::Rollback
+    render json: {
+      message: 'Unable to create reservation. Internal Server Error.',
+      error_list: 'Unable to create reservation. ActiveRecord::Rollback [1x602]'
+    }, status: 501
+  rescue ActiveRecord::StatementInvalid
+    render json: {
+      message: 'Unable to create reservation. Internal Server Error.',
+      error_list: 'Unable to create reservation. ActiveRecord::StatementInvalid [1x603]'
+    }, status: 501
+  rescue ActiveRecord::RecordInvalid => e
+    render json: {
+      message: 'Error saving user to database',
+      error_list: e.record.errors.full_messages
+    }, status: 501
   end
 
   def update_rooms_destruction(available_dates_list, reserved_rooms)
@@ -150,52 +134,47 @@ class V1::ReservationController < ApplicationController
   end
 
   def destroy
-    token = request.headers['token']
-    secret = Rails.application.secret_key_base.to_s
-    begin
-      decoded_token = JWT.decode token, secret, true, { algorithm: 'HS256' }
-      decoded_username = decoded_token[0]['name']
-      user = User.where(name: decoded_username).first
-      reservation_id = params[:id]
-      target_reservation = Reservation.find(reservation_id)
-      if user.id == target_reservation.user_id
-        available_on_date_for_hotel_list = AvailableOnDate.where(
-          hotel_id: target_reservation.hotel_id,
-          date: target_reservation.date_from..target_reservation.date_to
-        )
+    decoded_username = JsonWebToken.decode request.headers['token']
+    user = User.where(name: decoded_username).first
+    reservation_id = params[:id]
+    target_reservation = Reservation.find(reservation_id)
+    if user.id == target_reservation.user_id
+      available_on_date_for_hotel_list = AvailableOnDate.where(
+        hotel_id: target_reservation.hotel_id,
+        date: target_reservation.date_from..target_reservation.date_to
+      )
 
-        ActiveRecord::Base.transaction do
-          update_rooms_destruction(available_on_date_for_hotel_list, target_reservation.reserved_rooms)
+      ActiveRecord::Base.transaction do
+        update_rooms_destruction(available_on_date_for_hotel_list, target_reservation.reserved_rooms)
 
-          target_reservation.destroy
-          if target_reservation.destroyed?
-            render json: {
-              message: 'Reservation successfully destroyed'
-            }, status: 200
-          else
-            render json: {
-              error: 'Unable to delete reservation'
-            }, status: 501
-          end
+        target_reservation.destroy
+        if target_reservation.destroyed?
+          render json: {
+            message: 'Reservation successfully destroyed'
+          }, status: 200
+        else
+          render json: {
+            error: 'Unable to delete reservation'
+          }, status: 501
         end
-      else
-        render json: {
-          error: 'User not authorized'
-        }, status: 501
       end
-    rescue JWT::DecodeError
+    else
       render json: {
-        error: 'Invalid Token'
-      }, status: 500
-    rescue ActiveRecord::Rollback
-      render json: {
-        error: 'Unable to create reservation. ActiveRecord::Rollback [1x602]'
-      }, status: 501
-    rescue ActiveRecord::StatementInvalid
-      render json: {
-        error: 'Unable to create reservation. ActiveRecord::StatementInvalid [1x603]'
+        error: 'User not authorized'
       }, status: 501
     end
+  rescue JWT::DecodeError
+    render json: {
+      error: 'Invalid Token'
+    }, status: 500
+  rescue ActiveRecord::Rollback
+    render json: {
+      error: 'Unable to create reservation. ActiveRecord::Rollback [1x602]'
+    }, status: 501
+  rescue ActiveRecord::StatementInvalid
+    render json: {
+      error: 'Unable to create reservation. ActiveRecord::StatementInvalid [1x603]'
+    }, status: 501
   end
 
   private
