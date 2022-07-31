55584e114943fbab309994f80ed9e4dfbed98dee
Saadat Ali <saadatali0202@gmail.com>
2022-07-31 19:25:02 +0500

    Follow SOLID and DRY principles

--- app/controllers/v1/hotels_controller.rb
+++ app/controllers/v1/hotels_controller.rb
@@ -1,79 +1,72 @@
 class V1::HotelsController < ApplicationController
-  def index
-    token = request.headers['token']
-    secret = Rails.application.secret_key_base.to_s
-    begin
-      JWT.decode token, secret, true, { algorithm: 'HS256' }
-      hotels = Hotel.includes(image_attachment: :blob).limit(limit).offset(params[:offset])
-      hotels_list = hotels.map do |hotel|
-        image_url = ''
-        image_url = url_for(hotel.image) if hotel.image.attached?
-        hotel.as_json(
-          include: { feature: { except: %i[created_at updated_at] },
-                     address: { except: %i[created_at
-                                           updated_at] } }, except: %i[created_at updated_at feature_id address_id]
-        ).merge(
-          image_path: image_url
-        )
-      end
-      render json: {
-        data: hotels_list
-      }
-    rescue JWT::DecodeError
-      render json: {
-        error: 'Invalid Token'
-      }, status: 500
+  def hotels
+    hotels = Hotel.includes(image_attachment: :blob).limit(limit).offset(params[:offset])
+    hotels.map do |hotel|
+      image_url = ''
+      image_url = url_for(hotel.image) if hotel.image.attached?
+      hotel.as_json(
+        include: { feature: { except: %i[created_at updated_at] },
+                   address: { except: %i[created_at
+                                         updated_at] } }, except: %i[created_at updated_at feature_id address_id]
+      ).merge(
+        image_path: image_url
+      )
     end
   end
 
+  def index
+    JsonWebToken.decode request.headers['token']
+    render json: {
+      data: hotels
+    }
+  rescue JWT::DecodeError
+    render json: {
+      error: 'Invalid Token'
+    }, status: 500
+  end
+
+  def create_feature(parameters)
+    Feature.create!(room: parameters[:room], pool: parameters[:pool], bar: parameters[:bar],
+                    air_conditioning: parameters[:air_conditioning],
+                    tv: parameters[:tv], gym: parameters[:gym])
+  end
+
+  def create_address(parameters)
+    Address.create!(country: parameters[:country], state: parameters[:state], city: parameters[:city],
+                    neighbourhood: parameters[:neighbourhood], street: parameters[:street],
+                    number: parameters[:number], complement: parameters[:complement])
+  end
+
   def create
-    token = request.headers['token']
-    secret = Rails.application.secret_key_base.to_s
-    begin
-      decoded_token = JWT.decode token, secret, true, { algorithm: 'HS256' }
-      username_from_token = decoded_token[0]['name']
-      user = User.where(name: username_from_token).first
-      parameters = hotels_params
-      feature = Feature.create(room: parameters[:room], pool: parameters[:pool], bar: parameters[:bar],
-                               air_conditioning: parameters[:air_conditioning],
-                               tv: parameters[:tv], gym: parameters[:gym])
-      if feature.new_record?
-        render json: {
-          error: 'Unable to save to the database[1x501]',
-          error_list: feature.errors.full_messages
-        }, status: 501
-      else
-        address = Address.create(country: parameters[:country], state: parameters[:state], city: parameters[:city],
-                                 neighbourhood: parameters[:neighbourhood], street: parameters[:street],
-                                 number: parameters[:number], complement: parameters[:complement])
-        if address.new_record?
-          render json: {
-            error: 'Unable to save to the database[1x502]',
-            error_list: address.errors.full_messages
-          }, status: 501
-          feature.destroy
-        else
-          hotel = Hotel.create(name: parameters[:name], description: parameters[:description], feature_id: feature.id,
-                               address_id: address.id, user_id: user.id, image: parameters[:image])
-          if hotel.new_record?
-            render json: {
-              error: 'Unable to save to the database[1x503]',
-              error_list: hotel.errors.full_messages
-            }, status: 501
-            feature.destroy
-            address.destroy
-          else
-            render json: {
-              message: 'Hotel created successfully'
-            }, status: 200
-          end
-        end
-      end
-    rescue JWT::DecodeError
+    username_from_token = JsonWebToken.decode request.headers['token']
+    user = User.where(name: username_from_token).first
+    parameters = hotels_params
+    ActiveRecord::Base.transaction do
+      feature = create_feature parameters
+      address = create_address parameters
+      Hotel.create(name: parameters[:name], description: parameters[:description], feature_id: feature.id,
+                   address_id: address.id, user_id: user.id, image: parameters[:image])
       render json: {
-        error: 'Invalid Token'
-      }, status: 500
+        message: 'Hotel created successfully'
+      }, status: 200
     end
+  rescue JWT::DecodeError
+    render json: {
+      error: 'Invalid Token'
+    }, status: 500
+  rescue ActiveRecord::Rollback
+    render json: {
+      error: 'Unable to create reservation. ActiveRecord::Rollback '
+    }, status: 501
+  rescue ActiveRecord::StatementInvalid
+    render json: {
+      error: 'Unable to create reservation. ActiveRecord::StatementInvalid'
+    }, status: 501
+  rescue ActiveRecord::RecordInvalid => e
+    render json: {
+      message: 'Error saving user to database',
+      error_list: e.record.errors.full_messages
+    }, status: 501
   end
 
   private
